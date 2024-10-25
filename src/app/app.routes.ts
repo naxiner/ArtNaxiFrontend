ad9e510478e7923b9bec89c230cbef33d4c29508
naxiner <kirienkodenis2002@gmail.com>
2024-10-25 17:17:00 +0300

    Add UserProfile to Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { UserProfileComponent } from './user-profile/user-profile.component';
 
 export const routes: Routes = [
-    {path: 'user/register', component: RegisterComponent},
-    {path: 'user/login', component: LoginComponent}
+    { path: 'user/register', component: RegisterComponent },
+    { path: 'user/login', component: LoginComponent },
+    { path: 'profile/:id', component: UserProfileComponent }
 ];
