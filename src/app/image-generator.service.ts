683505c45fbff8b304e1c32020845443f0b17c95
naxiner <kirienkodenis2002@gmail.com>
2024-10-26 23:15:58 +0300

    Add ImageGenerator Service

---
+++ src/app/image-generator.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { SDRequest } from '../models/sd-request';
+import { SDResponse } from '../models/sd-response';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ImageGeneratorService {
+  private apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) { }
+
+  generateImage(request: SDRequest): Observable<SDResponse> {
+    const token = localStorage.getItem('authToken');
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    
+    return this.http.post<SDResponse>(`${this.apiUrl}/image`, request, { headers });
+  } 
+}
