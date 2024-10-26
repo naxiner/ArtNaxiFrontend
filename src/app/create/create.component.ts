ed881d91f99c37bc80a166a258196bdd0e850488
naxiner <kirienkodenis2002@gmail.com>
2024-10-26 23:18:28 +0300

    Add Create component

---
+++ src/app/create/create.component.ts
@@ -0,0 +1,52 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ImageGeneratorService } from '../image-generator.service';
+import { SDRequest } from '../../models/sd-request';
+import { CommonModule } from '@angular/common';
+import { environment } from '../../environments/environment';
+
+@Component({
+  selector: 'app-create',
+  standalone: true,
+  imports: [FormsModule, CommonModule],
+  templateUrl: './create.component.html',
+  styleUrl: './create.component.css'
+})
+export class CreateComponent {
+  baseUrl = environment.baseUrl;
+
+  sdRequest: SDRequest = {
+    prompt: '',
+    negative_prompt: '',
+    styles: [],
+    sampler_name: 'DPM++ SDE',
+    scheduler: 'Karras',
+    steps: 7,
+    cfg_scale: 2,
+    width: 512,
+    height: 512
+  }
+
+  styles: string[] = ['Negative'];
+
+  generatedImageUrl: string | null = null;
+
+  constructor(private sdService: ImageGeneratorService) {}
+
+  onSubmit(): void {
+    this.sdRequest.styles = this.sdRequest.styles || [];
+    if (!Array.isArray(this.sdRequest.styles)) {
+      this.sdRequest.styles = [this.sdRequest.styles];
+    }
+    
+    this.sdService.generateImage(this.sdRequest).subscribe({
+      next: (response) => {
+        console.log(response);
+        this.generatedImageUrl = response.imagePath;
+      },
+      error: (err) => {
+        console.log(err.error);
+      }
+    })
+  }
+}
