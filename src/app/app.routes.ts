5911c000759db59e88e231447ece2ae6d12571b4
naxiner <kirienkodenis2002@gmail.com>
2024-10-26 23:18:55 +0300

    Add Create to Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { CreateComponent } from './create/create.component';
 
 export const routes: Routes = [
     { path: 'user/register', component: RegisterComponent },
     { path: 'user/login', component: LoginComponent },
-    { path: 'profile/:id', component: UserProfileComponent }
+    { path: 'profile/:id', component: UserProfileComponent },
+    { path: 'create', component: CreateComponent }
 ];
