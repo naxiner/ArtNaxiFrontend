c1b4b0e2751fe52505c06a5163653b5dd2d1ac18
naxiner <kirienkodenis2002@gmail.com>
2024-10-30 15:34:02 +0200

    Add authGuard (login, register)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,11 @@ import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { CreateComponent } from './create/create.component';
+import { authGuard } from './auth.guard';
 
 export const routes: Routes = [
-    { path: 'user/register', component: RegisterComponent },
-    { path: 'user/login', component: LoginComponent },
+    { path: 'user/register', component: RegisterComponent, canActivate: [authGuard] },
+    { path: 'user/login', component: LoginComponent, canActivate: [authGuard] },
     { path: 'profile/:id', component: UserProfileComponent },
     { path: 'create', component: CreateComponent }
 ];
