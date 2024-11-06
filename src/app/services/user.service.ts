1c6bff02d96688488a2cb2a0113f5a10e637cca5
naxiner <kirienkodenis2002@gmail.com>
2024-11-06 21:22:07 +0200

    Add User Service

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { EditUserRequest } from '../../models/edit-user-request';
+import { environment } from '../../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private apiUrl = `${environment.apiUrl}/user`;
+
+  constructor(private http: HttpClient) { }
+
+  editUser(id: string, request: EditUserRequest): Observable<any> {
+    const token = localStorage.getItem('authToken');
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    
+    return this.http.put<any>(`${this.apiUrl}/${id}`, request, { headers });
+  }
+}
