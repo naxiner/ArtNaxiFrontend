b05ce35348c95ddc793ddc56d99a2d8f6d334013
naxiner <kirienkodenis2002@gmail.com>
2024-11-07 14:26:39 +0200

    Add updateProfileAvatar to User-Profile Service

--- src/app/services/user-profile.service.ts
+++ src/app/services/user-profile.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../environments/environment';
@@ -17,5 +17,12 @@ export class UserProfileService {
 
   getUserProfileAvatar(id: string): Observable<any> {
     return this.http.get<any>(`${this.apiUrl}/avatar/${id}`);
+  }
+
+  updateProfileAvatar(id: string, formData: FormData): Observable<any> {
+    const token = localStorage.getItem('authToken');
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+
+    return this.http.put<any>(`${this.apiUrl}/avatar/${id}`, formData, { headers });
   }
 }
