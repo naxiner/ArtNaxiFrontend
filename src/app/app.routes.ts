fc1e5454fcd5219c4527d38d37961f3577e3500a
naxiner <kirienkodenis2002@gmail.com>
2024-11-15 12:29:13 +0200

    Add Style to routes and navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { UserManagementComponent } from './components/user-management/user-management.component';
+import { StyleComponent } from './components/style/style.component';
 import { RegisterComponent } from './components/register/register.component';
 import { LoginComponent } from './components/login/login.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
@@ -7,11 +9,11 @@ import { UserProfileComponent } from './components/user-profile/user-profile.com
 import { CreateComponent } from './components/create/create.component';
 import { guestGuard } from './guards/guest.guard';
 import { authGuard } from './guards/auth.guard';
-import { UserManagementComponent } from './components/user-management/user-management.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'admin/users', component: UserManagementComponent},
+    { path: 'admin/styles', component: StyleComponent},
     { path: 'user/register', component: RegisterComponent, canActivate: [guestGuard] },
     { path: 'user/login', component: LoginComponent, canActivate: [guestGuard] },
     { path: 'profile/:id', component: UserProfileComponent },
