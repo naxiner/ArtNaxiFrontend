a75787aa85cb615657d130863b30ded697b5dd60
naxiner <kirienkodenis2002@gmail.com>
2024-11-19 01:16:44 +0200

    Install Toastr module

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,12 +4,14 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
 import { authInterceptor } from './interceptors/auth.interceptor';
 import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
+import { provideToastr, ToastrModule } from 'ngx-toastr';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }), 
     provideRouter(routes),
     provideHttpClient(withInterceptors([authInterceptor])),
+    provideToastr(),
     importProvidersFrom([SweetAlert2Module.forRoot()])
   ]
 };
